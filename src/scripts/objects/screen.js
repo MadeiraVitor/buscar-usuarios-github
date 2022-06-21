3f16027dbc64f68dbdc2c1471eae65cea4c9a8ea
MadeiraVitor <vitorsoutom@hotmail.com>
2022-06-21 08:10:54 -0300

    correção arquivo screen.js

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -2,12 +2,12 @@ const screen = {
     userProfile: document.querySelector('.profile-data'),
     renderUser(user){
         this.userProfile.innerHTML = `<div class="info">
-                             <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
-                             <div class="data">
-                                 <h1>${user.name ?? 'Não possui nome cadastrado'}</h1>
-                                 <p>${user.bio ?? 'Não possui bio cadastrada'}</p>
-                             </div>
-                        </div>`
+                                        <img src="${user.avatarUrl}" alt="Foto do perfil do usuário" />
+                                        <div class="data">
+                                            <h1>${user.name ?? 'Não possui nome cadastrado'}</h1>
+                                            <p>${user.bio ?? 'Não possui bio cadastrada'}</p>
+                                        </div>
+                                      </div>`
 
         let repositoriesItens = ''
         user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
